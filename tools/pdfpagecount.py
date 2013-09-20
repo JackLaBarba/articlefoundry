a5f937c64b9ba096bd1f3468f77e899e7746844a
Jack LaBarba <brakit@gmail.com>
2013-09-20 08:53:08 -0700

    WIP

---
+++ tools/pdfpagecount.py
@@ -0,0 +1,8 @@
+from ..articlefoundry import Article
+
+parser = argparse.ArgumentParser(description="Get the pdf page count from an "
+                                 "article package")
+
+if __name__=='__main__':
+    pass
+    
