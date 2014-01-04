ac51f2e7b25c4fe6f9ba35a5b5c5a6ffa2ba8227
Jack LaBarba <brakit@gmail.com>
2014-01-03 17:24:48 -0800

    get rid of pdfpagecount.py

--- tools/pdfpagecount.py
+++
@@ -1,8 +0,0 @@
-from ..articlefoundry import Article
-
-parser = argparse.ArgumentParser(description="Get the pdf page count from an "
-                                 "article package")
-
-if __name__=='__main__':
-    pass
-    
